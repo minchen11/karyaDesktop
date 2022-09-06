110061cca5320594a4c431cab1414f52a7406405
A_Decrement <76158970+minchen11@users.noreply.github.com>
2022-09-06 23:35:22 +0700

    Add files via upload

---
+++ FilmDanJadwal.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TiketBioskop
+{
+    partial class FilmDanJadwal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CBXNama = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CBXTayang = new System.Windows.Forms.ComboBox();
+            this.PB1 = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.BukaFoto = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.PB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CBXNama
+            // 
+            this.CBXNama.FormattingEnabled = true;
+            this.CBXNama.Location = new System.Drawing.Point(221, 336);
+            this.CBXNama.Margin = new System.Windows.Forms.Padding(4);
+            this.CBXNama.Name = "CBXNama";
+            this.CBXNama.Size = new System.Drawing.Size(112, 24);
+            this.CBXNama.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 316);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NAMA FILM";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 259);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "JAM TAYANG";
+            // 
+            // CBXTayang
+            // 
+            this.CBXTayang.FormattingEnabled = true;
+            this.CBXTayang.Location = new System.Drawing.Point(221, 279);
+            this.CBXTayang.Margin = new System.Windows.Forms.Padding(4);
+            this.CBXTayang.Name = "CBXTayang";
+            this.CBXTayang.Size = new System.Drawing.Size(112, 24);
+            this.CBXTayang.TabIndex = 3;
+            // 
+            // PB1
+            // 
+            this.PB1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.PB1.Location = new System.Drawing.Point(183, 48);
+            this.PB1.Name = "PB1";
+            this.PB1.Size = new System.Drawing.Size(207, 187);
+            this.PB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PB1.TabIndex = 5;
+            this.PB1.TabStop = false;
+            this.PB1.Click += new System.EventHandler(this.PB1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(13, 13);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(81, 16);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Admin Page";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // BukaFoto
+            // 
+            this.BukaFoto.FileName = "BukaFoto";
+            // 
+            // FilmDanJadwal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 554);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.PB1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CBXTayang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CBXNama);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FilmDanJadwal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PB1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CBXNama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CBXTayang;
+        private System.Windows.Forms.PictureBox PB1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.OpenFileDialog BukaFoto;
+    }
+}
---
+++ admin.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace TiketBioskop
+{
+    public partial class admin : Form
+    {
+        readonly Koneksi conn = new Koneksi();
+        DataSet data = new DataSet();
+
+        public admin()
+        {
+            InitializeComponent();
+            TampilkanData();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            conn.koneksi.Open();
+            MySqlDataAdapter DataFilm = new MySqlDataAdapter("select * from film", conn.koneksi);
+        }
+
+        private void BTN_Studio_Click(object sender, EventArgs e)
+        {
+            HLMN_Studio.BringToFront();
+           
+        }
+
+        private void BTN_akun_Click(object sender, EventArgs e)
+        {
+            HLMN_Akun.BringToFront();
+        }
+
+        private void Film_Click(object sender, EventArgs e)
+        {
+            HLMN_Film.BringToFront();
+        }
+
+       void TampilkanData()
+        {
+            dataGridView1.DataSource = conn.showdata("account");
+            dataGridView1.Columns[0].HeaderText = "Id anggota";
+            dataGridView1.Columns[1].HeaderText = "Nama";
+            dataGridView1.Columns[2].HeaderText = "Username";
+            dataGridView1.Columns[3].HeaderText = "Password";
+            dataGridView1.Columns[4].HeaderText = "No Hp";
+
+        }
+
+        private void PB1_Click(object sender, EventArgs e)
+        {
+            BukaFoto.Filter = "*.jpg ; *.png ;*.*|";
+            BukaFoto.ShowDialog();
+            PB1.SizeMode = PictureBoxSizeMode.Zoom;
+            PB1.ImageLocation = BukaFoto.FileName;
+        }
+
+       
+
+        private void dataGridView2_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            conn.koneksi.Open();
+            TXT_JudulFilm.Text = dataGridView2.Rows[e.RowIndex].Cells[0].Value.ToString();
+            TXT_JamTayang.Text = dataGridView2.Rows[e.RowIndex].Cells[1].Value.ToString();
+            DTP_JadwalTayang.Text = dataGridView2.Rows[e.RowIndex].Cells[2].Value.ToString();
+            conn.koneksi.Close();
+        }
+
+        private void BTN_TambahFilm_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string create = "insert into film (nama_film,jadwal_tayang,jam_tayang,poster) values (nama_film='"+TXT_JudulFilm.Text+"',jadwal_tayang='"+ DTP_JadwalTayang.Text+ "',jam_tayang='"+TXT_JamTayang+"'";
+                conn.koneksi.Open();
+                MySqlCommand query = new MySqlCommand(create, conn.koneksi);
+
+                query.Prepare();
+                MySqlDataAdapter dataadapter = new MySqlDataAdapter(query);
+                query.ExecuteNonQuery();
+                MessageBox.Show("Berhasil");
+                conn.koneksi.Close();
+                TampilkanData();
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("gagal");
+            }
+        }
+    }
+}
